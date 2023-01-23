9d65b5af15b47f056340393ec901a9e77ca7efcc
Theseus <heavenlywork@outlook.com>
2023-01-23 22:06:05 +0300

    add comments

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/App.css';
 import MyButton from "./UI/button/MyButton";
-import {useNavigate, useParams} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const PostItem = (props) => {
     const router = useNavigate();
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -3,26 +3,50 @@ import {useParams} from "react-router-dom";
 import {useFetching} from "../hooks/useFetching";
 import PostService from "../API/PostService";
 import Loader from "../components/UI/loader/Loader";
+import {findAllByDisplayValue} from "@testing-library/react";
 
 const PostIdPage = () => {
     const params = useParams();
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id)
-        console.log(response)
         setPost(response.data)
+    });
+    const [fetchCommentsById, isCommentsLoading, commentError] = useFetching(async (id) => {
+        const response = await PostService.getCommentById(id)
+        setComments(response.data)
     });
 
     useEffect(() => {
         fetchPostById(params.id)
+        fetchCommentsById(params.id)
     }, [])
 
     return (
         <div>
-            <div>{post.id}</div>
+            <h1>{'Post no:' + post.id}</h1>
             {isLoading
                 ? <Loader/>
-                : <div>{post.title} {post.body}</div>}
+                : <div>
+                    <h4>{post.title}</h4>
+                    <div>{post.body}</div>
+                </div>
+            }
+            <h2 style={{margin: 10}}>{'Comments'}</h2>
+            {isCommentsLoading
+                ? <Loader/>
+                : <div>
+                    {comments.map(comment =>
+                        <div key={comment.id} style={{marginTop: 15}}>
+                            <h3>{comment.email}</h3>
+                            <div>{comment.body}</div>
+                        </div>)}
+                </div>
+
+
+            }
+
         </div>
     );
 };
